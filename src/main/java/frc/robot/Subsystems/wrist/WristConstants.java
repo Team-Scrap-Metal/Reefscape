0e1f3a840de3b7eda5662e7674371e06f69c80cd
V8Climb8D <rtclimb34@gmail.com>
2025-03-13 18:45:55 -0700

    Added PID To Wrist WIP: Needs Testing

--- src/main/java/frc/robot/Subsystems/wrist/WristConstants.java
+++ src/main/java/frc/robot/Subsystems/wrist/WristConstants.java
@@ -1,9 +1,18 @@
 package frc.robot.Subsystems.wrist;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Unit;
+
 public class WristConstants {
     public static final int CAN_ID = 19;
     public static final boolean IS_INVERTED = false;
     public static final int STALL_LIMIT_AMPS = 40;
     public static final int FREE_SPIN_LIMIT_AMPS = 40;
     public static final double GEAR_RATIO = 40;
+
+    public static final double kP = 0.0;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+    public static final double PID_TOLERANCE_RAD = Units.degreesToRadians(1);
+
 }
